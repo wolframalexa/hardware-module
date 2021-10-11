c9786d8679eae119b1c6d0208cdc0741ade8bf0d
wolframalexa <alexajakob@tutanota.com>
2021-10-10 20:02:39 -0400

    write sample code

--- blazepose_sample.py
+++ blazepose_sample.py
@@ -1,44 +1,24 @@
 # sample code copied from: https://www.tensorflow.org/lite/guide/inference#load_and_run_a_model_in_python
 
-class TestModel(tf.Module):
-  def __init__(self):
-    super(TestModel, self).__init__()
+import numpy as np
+import tensorflow as tf
 
-  @tf.function(input_signature=[tf.TensorSpec(shape=[1, 10], dtype=tf.float32)])
-  def add(self, x):
-    '''
-    Simple method that accepts single input 'x' and returns 'x' + 4.
-    '''
-    # Name the output 'result' for convenience.
-    return {'result' : x + 4}
+# Load the TFLite model and allocate tensors.
+interpreter = tf.lite.Interpreter(model_path="pose_detection.tflite")
+interpreter.allocate_tensors()
 
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
 
-SAVED_MODEL_PATH = 'content/saved_models/test_variable'
-TFLITE_FILE_PATH = 'content/test_variable.tflite'
+# Test the model on random input data.
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
+interpreter.set_tensor(input_details[0]['index'], input_data)
 
-# Save the model
-module = TestModel()
-# You can omit the signatures argument and a default signature name will be
-# created with name 'serving_default'.
-tf.saved_model.save(
-    module, SAVED_MODEL_PATH,
-    signatures={'my_signature':module.add.get_concrete_function()})
+interpreter.invoke()
 
-# Convert the model using TFLiteConverter
-converter = tf.lite.TFLiteConverter.from_saved_model(SAVED_MODEL_PATH)
-tflite_model = converter.convert()
-with open(TFLITE_FILE_PATH, 'wb') as f:
-  f.write(tflite_model)
-
-# Load the TFLite model in TFLite Interpreter
-interpreter = tf.lite.Interpreter(TFLITE_FILE_PATH)
-# There is only 1 signature defined in the model,
-# so it will return it by default.
-# If there are multiple signatures then we can pass the name.
-my_signature = interpreter.get_signature_runner()
-
-# my_signature is callable with input as arguments.
-output = my_signature(x=tf.constant([1.0], shape=(1,10), dtype=tf.float32))
-# 'output' is dictionary with all outputs from the inference.
-# In this case we have single output 'result'.
-print(output['result'])
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
